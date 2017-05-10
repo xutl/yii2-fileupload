034b065fdb18b57e08d0f8b4938bf1347d8f8398
xuchao <85825770@qq.com>
2017-05-10 12:56:52 +0800

    update

--- UploadAction.php
+++ UploadAction.php
@@ -4,7 +4,7 @@
  * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
  * @license http://www.tintsoft.com/license/
  */
-namespace xutl\fileupload\actions;
+namespace xutl\fileupload;
 
 use Yii;
 use yii\base\Action;
